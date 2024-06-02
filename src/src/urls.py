1c65f08fcc782da518a2cf806a58ce3a42cbc754
Shubham Jha <s.jha7777@gmail.com>
2024-06-02 13:18:08 +0530

    Add login logout URLs

--- src/src/urls.py
+++ src/src/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path("", include("blog.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/login/", views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", views.LogoutView.as_view(), name="logout"),
 ]
 
 if settings.DEBUG:
